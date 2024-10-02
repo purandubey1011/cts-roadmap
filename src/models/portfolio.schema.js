8273feb7cce4b19833a757598aa47b893c7e3e0b
purandubey1011 <dubeypuran2002@gmail.com>
2024-10-02 00:32:44 -0700

    admin can create update delete portfolios.

---
+++ src/models/portfolio.schema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const PortfolioSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    livelink: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    purchased: {
+        type: [],
+        default: []
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    video: {
+        type: Object,
+        default: {
+            fileId: "",
+            url: "",
+        },
+    },
+}, { timestamps: true });
+
+const Portfolio = mongoose.model('Portfolio', PortfolioSchema);
+
+module.exports = Portfolio;
