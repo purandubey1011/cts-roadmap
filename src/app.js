975ac88ece74efb65465e1454eab020f1c45dc91
purandubey1011 <dubeypuran2002@gmail.com>
2024-10-21 02:37:01 -0700

    change cors origin

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const cors = require("cors");
 
 app.use(
     cors({
-        origin: ["https://crosstheskylimits.online", "http://localhost:5173"],
+        origin: ["https://crosstheskylimits.online","https://www.crosstheskylimits.online","https://api.crosstheskylimits.online","http://localhost:5173"],
         credentials: true, 
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization']
