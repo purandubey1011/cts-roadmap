eb02c6007cd304ee478f1404e92f9e8db6a775e5
purandubey1011 <dubeypuran2002@gmail.com>
2024-10-21 06:14:51 -0700

    mail content added

--- src/controllers/exclusive.controllers.js
+++ src/controllers/exclusive.controllers.js
@@ -307,7 +307,7 @@ exports.payment_success_commonapp = catchAsyncErrors(async (req, res, next) => {
     const mailOptions = {
       from: "Cross The Skylimits.",
       to: user.email,
-      subject: "Welcome to Our Essay Editing Service",
+      subject: "Congratulations! Your Common App Review is Confirmed",
       html: `
       <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #e2e8f0; border-radius: 8px;">
         <h1 style="font-size: 24px; font-weight: bold; color: #1a202c; margin-bottom: 20px;">Welcome to Our Essay Editing Service!</h1>
@@ -466,28 +466,41 @@ exports.payment_success_cssprofile = catchAsyncErrors(async (req, res, next) => 
     const mailOptions = {
       from: "Cross The Skylimits.",
       to: user.email,
-      subject: "Welcome to Our Essay Editing Service",
+      subject: "Congratulations! Your CSS Profile Support is Confirmed!",
       html: `
       <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #e2e8f0; border-radius: 8px;">
-        <h1 style="font-size: 24px; font-weight: bold; color: #1a202c; margin-bottom: 20px;">Welcome to Our Essay Editing Service!</h1>
-        <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">Dear ${user.name},</p>
-        <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
-          Thank you for choosing our Essay Editing service! We’re excited to embark on this journey with you to craft an essay that truly reflects your unique voice and story. Our team of experts, including top Ivy League students and seasoned professionals, is committed to providing you with the best guidance and support. And you will get your edited essay within 24hrs.
-        </p>
-        <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
-          Rest assured, your essay is in capable hands. We pride ourselves on delivering results that exceed expectations. However, if at any point you feel that the outcome doesn’t meet your satisfaction, please don’t hesitate to DM or contact us directly. Your satisfaction is our top priority, and we’re here to ensure you feel confident in your submission.
-        </p>
-        <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
-          We look forward to helping you shine through your essay!
-        </p>
-        <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
-          Best regards,<br>
-          The Essay Editing Team
-        </p>
+          <h1 style="font-size: 24px; font-weight: bold; color: #1a202c; margin-bottom: 20px;">Congratulations! Your CSS Profile Support is Confirmed!</h1>
+          <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">Dear ${user.name},</p>
+          <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
+              We’re thrilled to welcome you to our CSS Profile Support service! This essential part of your financial aid journey can be complex, but with our expert guidance, you’ll navigate the process smoothly.
+          </p>
+          <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
+              Our team will help you:
+          </p>
+          <ul style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
+              <li>Understand and fill out your CSS Profile.</li>
+              <li>Identify and correct any mistakes.</li>
+              <li>Complete the profile to maximize your chances of receiving aid.</li>
+          </ul>
+          <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
+              Here’s how to proceed:
+          </p>
+          <ol style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
+              <li>Schedule your first consultation with our expert, <a href="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0G8fTaMy33MET908cjrTeJgCSrKH7xSG2jDbBfo_iMlJE8ELSInzJUjzUVdqEC8ELbgUIBgoFS" target="_blank">Schedule here</a>.</li>
+              <li>Work with our team to fill out your profile, review, and finalize it.</li>
+              <li>Submit your completed CSS Profile for financial aid consideration.</li>
+          </ol>
+          <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
+              We’ll be with you every step of the way to ensure your profile is fully optimized. Feel free to reach out if you have any questions.
+          </p>
+          <p style="font-size: 16px; color: #4a5568; margin-bottom: 20px;">
+              Thank you,<br>
+              The CSS Profile Support Team
+          </p>
       </div>
-    `,
-    };
-
+      `,
+  };
+    
     transport.sendMail(mailOptions, (err, info) => {
       if (err) return next(new ErrorHandler(err, 500));
       res
